cd848ae502bcfb92af416ec2a268ec9771d0893c
Alina Kornilova <alinakornilova78@gmail.com>
2020-01-29 13:49:20 +0000

    Automatically backed up by Learn

--- db/migrate/coupons.rb
+++ db/migrate/coupons.rb
@@ -1,8 +1,8 @@
-class Coupons < ActiveRecord::Migration
+class Coupon < ActiveRecord::Migration
   def change
     create_table :coupons do |t|
-      t.string :first_name, null: false
-      t.string :last_name, null: false
+      t.string :coupon_code, null: false
+      t.string :store, null: false
 
       t.timestamps null: false
     end
